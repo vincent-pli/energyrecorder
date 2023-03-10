aff2a091edc534a49a84b5f769b38eacc1981b32
vincent-pli <justdoit.pli@gmail>
2023-03-10 00:24:01 -0500

    change type

--- recording-api/service/mqtt.py
+++ recording-api/service/mqtt.py
@@ -113,7 +113,7 @@ class MQTTService:
                 #     settings.MQTT["port"],
                 # )
                 self._mqtt_client = wiotp.sdk.device.DeviceClient(
-                    config=self._complete_config("lenovoRS_Devicetype", equipement), logHandlers=None)
+                    config=self._complete_config("hpDL_Devicetype", equipement), logHandlers=None)
 
                 self._mqtt_client.commandCallback = self._commandCallback
 
